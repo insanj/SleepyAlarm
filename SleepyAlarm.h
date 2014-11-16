c38893ce424d5dc9a7ee82f12505b14d22292537
insanj <insanj@gmail.com>
2014-11-15 22:08:45 -0500

    slight header / log changes, :shipit:?

--- SleepyAlarm.h
+++ SleepyAlarm.h
@@ -3,7 +3,7 @@
 #import "substrate.h"
 
 #define SLSETTINGS [NSDictionary dictionaryWithContentsOfFile:[NSHomeDirectory() stringByAppendingPathComponent:@"/Library/Preferences/com.insanj.sleepyalarm.plist"]]
-#define SLLog(fmt, ...) NSLog((@"%s[SleepyAlarm]" fmt @"%s"), "\e[0;35m", ##__VA_ARGS__, "\x1B[0m")
+#define SLLog(fmt, ...) NSLog((@"%s[SleepyAlarm] " fmt @"%s"), "\e[1;35m", ##__VA_ARGS__, "\x1B[0m")
 
 @interface TableViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 
@@ -18,8 +18,8 @@
 
 @interface AlarmViewController (SleepyAlarm) <UIActionSheetDelegate>
 
-- (void)slShouldDarken:(BOOL)should;
 - (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex;
+- (void)sl_sleepyPress:(id)sender;
 
 @end
 
@@ -37,6 +37,6 @@
 
 @interface EditAlarmView : UIView
 
-@property(readonly, nonatomic) UIDatePicker *timePicker;
+@property (readonly, nonatomic) UIDatePicker *timePicker;
 
 @end
